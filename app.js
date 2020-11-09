4334b1f0d7476b6a5258e454e0153c2c0836f551
gregfozo <gregcook2011@gmail.com>
2020-11-09 16:06:43 -0600

    Update app.js
    minor changes

--- app.js
+++ app.js
@@ -6,21 +6,24 @@ puzzleEl.textContent = game1.getPuzzle();
 guessesEl.textContent = guessesEl.textContent = `Remaining guesses: ${game1.remainingGuesses}`;
 
 window.addEventListener('keypress', function(e){
-    game1.makeGuess(e.key)
 
     if (game1.remainingGuesses < 0 || !puzzleEl.textContent.includes("*")) {
         return;
         }
 
-    if(game1.getPuzzle().includes('*') && game1.remainingGuesses >= 1){
+    game1.makeGuess(e.key)
+
+    if(game1.getPuzzle().includes('*')){
         puzzleEl.textContent = game1.getPuzzle();
         guessesEl.textContent = `Remaining guesses: ${game1.remainingGuesses}`;
-    } else if(!game1.getPuzzle().includes('*') && game1.remainingGuesses >= 1) {
+    } else if(!game1.getPuzzle().includes('*')) {
         puzzleEl.textContent = "YOU WON!"
         guessesEl.textContent = "Congratulations!"
-    } else {
+    }
+
+    if(game1.remainingGuesses < 1){
         puzzleEl.textContent = "GAME OVER!"
-        guessesEl.textContent = `You lost :( The word was "${game1.word.join("")}"`
+        guessesEl.textContent = `You lost :( The word was "${game1.word.join("")}"`  
     }
     
 })
